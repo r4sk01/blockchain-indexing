110934688a41a87609361d23845f851971319c01
Andrei Bachinin <limbus727@gmail.com>
2023-06-21 20:37:27 -0600

    Changes to chaincode and application GO

--- blockchainIndexing/go/application.go
+++ blockchainIndexing/go/application.go
@@ -1,1 +1,186 @@
 package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
+	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type Table struct {
+	Table []Order `json:"table"`
+}
+
+type Order struct {
+	L_ORDERKEY      int     `json:"L_ORDERKEY"`
+	L_PARTKEY       int     `json:"L_PARTKEY"`
+	L_SUPPKEY       int     `json:"L_SUPPKEY"`
+	L_LINENUMBER    int     `json:"L_LINENUMBER"`
+	L_QUANTITY      int     `json:"L_QUANTITY"`
+	L_EXTENDEDPRICE float64 `json:"L_EXTENDEDPRICE"`
+	L_DISCOUNT      float64 `json:"L_DISCOUNT"`
+	L_TAX           float64 `json:"L_TAX"`
+	L_RETURNFLAG    string  `json:"L_RETURNFLAG"`
+	L_LINESTATUS    string  `json:"L_LINESTATUS"`
+	L_SHIPDATE      string  `json:"L_SHIPDATE"`
+	L_COMMITDATE    string  `json:"L_COMMITDATE"`
+	L_RECEIPTDATE   string  `json:"L_RECEIPTDATE"`
+	L_SHIPINSTRUCT  string  `json:"L_SHIPINSTRUCT"`
+	L_SHIPMODE      string  `json:"L_SHIPMODE"`
+	L_COMMENT       string  `json:"L_COMMENT"`
+}
+
+func main() {
+	os.Setenv("DISCOVERY_AS_LOCALHOST", "true")
+	wallet, err := gateway.NewFileSystemWallet("wallet")
+	if err != nil {
+		log.Fatalf("Failed to create wallet: %s\n", err)
+	}
+
+	if !wallet.Exists("appUser") {
+		err = populateWallet(wallet)
+		if err != nil {
+			log.Fatalf("Failed to populate wallet contents: %s\n", err)
+		}
+	}
+
+	ccpPath := filepath.Join(
+		"..",
+		"..",
+		"test-network",
+		"organizations",
+		"peerOrganizations",
+		"org1.example.com",
+		"connection-org1.yaml",
+	)
+
+	gw, err := gateway.Connect(
+		gateway.WithConfig(config.FromFile(filepath.Clean(ccpPath))),
+		gateway.WithIdentity(wallet, "appUser"),
+	)
+	if err != nil {
+		log.Fatalf("Failed to connect to gateway: %s\n", err)
+	}
+	defer gw.Close()
+
+	network, err := gw.GetNetwork("mychannel")
+	if err != nil {
+		log.Fatalf("Failed to get network: %s\n", err)
+	}
+
+	contract := network.GetContract("blockchainIndexing")
+
+	transaction := flag.String("t", "defaultQuery", "Choose a transaction to run")
+	file := flag.String("f", "~", "file path for json data")
+	flag.Parse()
+
+	switch *transaction {
+	case "BulkInvoke":
+		BulkInvoke(contract, *file)
+	case "getHistoryForAsset":
+		getHistoryForAsset(contract)
+	}
+}
+
+func populateWallet(wallet *gateway.Wallet) error {
+	credPath := filepath.Join(
+		"..",
+		"..",
+		"test-network",
+		"organizations",
+		"peerOrganizations",
+		"org1.example.com",
+		"users",
+		"User1@org1.example.com",
+		"msp",
+	)
+
+	certPath := filepath.Join(credPath, "signcerts", "cert.pem")
+	// read the certificate pem
+	cert, err := ioutil.ReadFile(filepath.Clean(certPath))
+	if err != nil {
+		return err
+	}
+
+	keyDir := filepath.Join(credPath, "keystore")
+	// there's a single file in this dir containing the private key
+	files, err := ioutil.ReadDir(keyDir)
+	if err != nil {
+		return err
+	}
+	if len(files) != 1 {
+		return errors.New("keystore folder should have contain one file")
+	}
+	keyPath := filepath.Join(keyDir, files[0].Name())
+	key, err := ioutil.ReadFile(filepath.Clean(keyPath))
+	if err != nil {
+		return err
+	}
+
+	identity := gateway.NewX509Identity("Org1MSP", string(cert), string(key))
+
+	err = wallet.Put("appUser", identity)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func BulkInvoke(contract *gateway.Contract, fileUrl string) {
+	if fileUrl == "" || !filepath.IsAbs(fileUrl) {
+		log.Fatalf("File URL must be provided and must be an absolute path")
+	}
+
+	jsonData, err := ioutil.ReadFile(fileUrl)
+	if err != nil {
+		log.Fatalf("error while reading json file: %s", err)
+	}
+
+	var table Table
+	if err := json.Unmarshal([]byte(jsonData), &table); err != nil {
+		log.Fatalf("Failed to unmarshal JSON: %s", err)
+	}
+
+	orders := table.Table
+
+	startTime := time.Now()
+	log.Printf("Starting bulk transaction at time: %s\n", startTime.Format(time.UnixDate))
+
+	//Split orders into chunks of size 1000
+	chunkSize := 1000
+	for i := 0; i < len(orders); i += chunkSize {
+		chunkTime := time.Now()
+
+		chunk := orders[i:func() int {
+			if i+chunkSize > len(orders) {
+				return len(orders)
+			}
+			return i + chunkSize
+		}()]
+
+		chunkBytes, err := json.Marshal(chunk)
+		if err != nil {
+			log.Fatalf("Failed to marshal JSON: %s", err)
+		}
+
+		_, err = contract.SubmitTransaction("CreateBulk", string(chunkBytes))
+		if err != nil {
+			log.Fatalf("Failed to submit transaction: %s\n", err)
+		}
+
+		endTime := time.Now()
+		executionTime := endTime.Sub(chunkTime).Seconds()
+		log.Printf("Execution Time: %f sec at chunk %d", executionTime, i/chunkSize+1)
+	}
+
+	endTime := time.Now()
+	executionTime := endTime.Sub(startTime).Seconds()
+	log.Printf("Finished bulk transaction at time: %s\n", endTime.Format(time.UnixDate))
+	log.Printf("Total execution time is: %f sec\n", executionTime)
+}
--- chaincode/blockchainIndexing/go/blockchainIndexing.go
+++ chaincode/blockchainIndexing/go/blockchainIndexing.go
@@ -1,1 +1,133 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"log"
+	"strconv"
+)
+
+// SmartContract provides functions for managing an Asset
+type SmartContract struct {
+	contractapi.Contract
+}
+
+type Asset struct {
+	TxId      string `json:"txId"`
+	Value     string `json:"value"`
+	Timestamp string `json:"timestamp"`
+}
+
+type Order struct {
+	L_ORDERKEY      int     `json:"L_ORDERKEY"`
+	L_PARTKEY       int     `json:"L_PARTKEY"`
+	L_SUPPKEY       int     `json:"L_SUPPKEY"`
+	L_LINENUMBER    int     `json:"L_LINENUMBER"`
+	L_QUANTITY      int     `json:"L_QUANTITY"`
+	L_EXTENDEDPRICE float64 `json:"L_EXTENDEDPRICE"`
+	L_DISCOUNT      float64 `json:"L_DISCOUNT"`
+	L_TAX           float64 `json:"L_TAX"`
+	L_RETURNFLAG    string  `json:"L_RETURNFLAG"`
+	L_LINESTATUS    string  `json:"L_LINESTATUS"`
+	L_SHIPDATE      string  `json:"L_SHIPDATE"`
+	L_COMMITDATE    string  `json:"L_COMMITDATE"`
+	L_RECEIPTDATE   string  `json:"L_RECEIPTDATE"`
+	L_SHIPINSTRUCT  string  `json:"L_SHIPINSTRUCT"`
+	L_SHIPMODE      string  `json:"L_SHIPMODE"`
+	L_COMMENT       string  `json:"L_COMMENT"`
+}
+
+type QueryResult struct {
+	Key    string `json:"Key"`
+	Record *Order
+}
+
+func (s *SmartContract) Init(stub shim.ChaincodeStubInterface) error {
+	fmt.Println("====== INIT CAllED ======")
+
+	return nil
+}
+
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	fmt.Println("====== INIT LEDGER START ======")
+	fmt.Println("====== INIT LEDGER END ======")
+
+	return nil
+}
+
+// CreateBulk Create a new key-value pair and send to state database
+func (s *SmartContract) CreateBulk(stub shim.ChaincodeStubInterface, args []string) error {
+	buffer := args[0]
+
+	var orders []Order
+	json.Unmarshal([]byte(buffer), &orders)
+
+	for _, order := range orders {
+
+		orderBytes, err := json.Marshal(order)
+		if err != nil {
+			return err
+		}
+
+		orderKey := strconv.FormatInt(int64(order.L_ORDERKEY), 10)
+
+		log.Printf("Appending order %s with part %d\n", orderKey, order.L_PARTKEY)
+		err = stub.PutState(orderKey, orderBytes)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// getHistoryForAsset executes getHistoryForKey() API
+func getHistoryForAsset(ctx contractapi.TransactionContextInterface, assetKey string) (string, error) {
+	historyResultsIterator, err := ctx.GetStub().GetHistoryForKey(assetKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to get asset history for %s: %v", assetKey, err)
+	}
+	defer historyResultsIterator.Close()
+
+	var history []Asset
+	for historyResultsIterator.HasNext() {
+		response, err := historyResultsIterator.Next()
+		if err != nil {
+			return "", fmt.Errorf("failed to iterate asset history: %v", err)
+		}
+
+		txTimestamp, err := ptypes.Timestamp(response.Timestamp)
+		if err != nil {
+			return "", fmt.Errorf("failed to parse timestamp: %v", err)
+		}
+
+		asset := Asset{
+			TxId:      response.TxId,
+			Value:     string(response.Value),
+			Timestamp: txTimestamp.String(),
+		}
+
+		history = append(history, asset)
+	}
+
+	historyAsJSON, err := json.Marshal(history)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal asset history: %v", err)
+	}
+
+	return string(historyAsJSON), nil
+}
+
+func main() {
+	chaincode, err := contractapi.NewChaincode(&SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating asset chaincode: %v", err)
+	}
+
+	if err := chaincode.Start(); err != nil {
+		log.Panicf("Error starting asset chaincode: %v", err)
+	}
+}
