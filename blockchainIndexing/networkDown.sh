779e67809c3982b1736870f8bf0a545593c78081
Daniel Garon <dgaron@colostate.edu>
2023-10-06 11:22:53 -0600

    Stop & start containers

--- blockchainIndexing/networkDown.sh
+++ blockchainIndexing/networkDown.sh
@@ -1,18 +1,2 @@
-#!/bin/bash
-#
-# Copyright IBM Corp All Rights Reserved
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-# Exit on first error
-set -ex
-
-# Bring the test network down
-pushd ../test-network
-./network.sh down
-popd
-
-# clean out any old identites in the wallets
-rm -rf javascript/wallet/*
-rm -rf java/wallet/*
-rm -rf typescript/wallet/*
+pushd ../test-network/docker
+docker-compose -f docker-compose-ca.yaml -f docker-compose-test-net.yaml stop
