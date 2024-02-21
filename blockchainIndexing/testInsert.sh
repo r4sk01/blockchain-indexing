9df09eb1357f8d3b5dde508145afba7785421730
Daniel Garon <dgaron@colostate.edu>
2024-02-21 10:33:39 -0700

    Insert script

--- blockchainIndexing/testInsert.sh
+++ blockchainIndexing/testInsert.sh
@@ -1,37 +1,52 @@
 #!/bin/bash
 
-results=insertResults-TPCH-1M.txt
+results=insertResults-TPCH-12M.txt
 
+dataFile=/home/andrey/Documents/insert-tpch/sortUnsort12KK/unsorted12KKEntries.json
 
+function insert() {
+    pushd /home/andrey/Documents/insert-tpch/blockchain-indexing/blockchainIndexing
+    echo "" >> "$results"
+    echo "PARALLEL" >> "$results"
+    for ((i = 0; i < 3; i++)); do
+        ./startFabric.sh go
+        sleep 10
+        pushd go
+        echo "Inserting $dataFile" >> ../"$results"
+        go run application.go -t BulkInvokeParallel -f "$dataFile" >> ../"$results" 2>&1
+        popd
+        ./networkDown.sh
+    done
+    popd
+}
+
+function buildImages() {
+    make docker-clean
+    echo "y" | docker image prune
+    make peer-docker
+    make orderer-docker
+}
+
+pushd /home/andrey/Desktop/fabric-rvp
+git checkout dgaron-2.3-blockRangeQueryOriginalIndex
+buildImages
+popd
 echo "" >> "$results"
 echo "ORIGINAL" >> "$results"
+insert
 
+pushd /home/andrey/Desktop/fabric-rvp
+git checkout dgaron-2.3-blockRangeQuery-VBI
+buildImages
+popd
 echo "" >> "$results"
-echo "PARALLEL" >> "$results"
-for ((i = 0; i < 3; i++)); do
-    ./startFabric.sh go
-    sleep 10
-    pushd go
+echo "VERSION" >> "$results"
+insert
 
-    for file in ${filenames[@]}; do
-        echo "Inserting file: /home/andrey/Documents/insert-tpch/sortUnsort10500/unsortedMilEntries.json"
-        go run application.go -t BulkInvokeParallel -f /home/andrey/Documents/insert-tpch/sortUnsort10500/unsortedMilEntries.json >> ../"$results" 2>&1
-    done
-
-    popd
-    ./networkDown.sh
-done
-
-# echo "" >> "$results"
-# echo "SEQUENTIAL" >> "$results"
-# ./original-startFabric.sh go
-# sleep 10
-# pushd go
-
-# for file in ${filenames[@]}; do
-#     echo "Inserting file: $dataDir/$file"
-#     go run application.go -t BulkInvoke -f "$dataDir/$file" >> ../"$results" 2>&1
-# done
-
-# popd
-# ./original-networkDown.sh
+pushd /home/andrey/Desktop/fabric-rvp
+git checkout dgaron-2.3-blockRangeQuery-BBI
+buildImages
+popd
+echo "" >> "$results"
+echo "BLOCK" >> "$results"
+insert
