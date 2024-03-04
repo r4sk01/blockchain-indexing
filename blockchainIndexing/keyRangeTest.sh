548110a95a5608dc42dbaf4302f8923bba05ea4f
Daniel Garon <dgaron@colostate.edu>
2024-03-04 12:02:40 -0700

    Range query test update

--- blockchainIndexing/keyRangeTest.sh
+++ blockchainIndexing/keyRangeTest.sh
@@ -39,26 +39,26 @@ main() {
 run_range_tests_1M() {
     local keyFile="~/Documents/insert-tpch/ethereum/1M-versions.txt"
     for _ in {1..6}; do
-        go run application.go -t getHistoryForAssetRange -k 0x000000000000d3b2c76221467d2f8c8f1de832a2 -r 100 -keylist "$keyFile"
+        go run application.go -t getHistoryForAssetRange -k 0x000000000000d3b2c76221467d2f8c8f1de832a2 -r 10 -keylist "$keyFile"
     done
     for _ in {1..6}; do
-        go run application.go -t getHistoryForAssetRange -k 0x03fb320c81ad2a55de600b13967879c341706afb -r 1000 -keylist "$keyFile"
+        go run application.go -t getHistoryForAssetRange -k 0x03fb320c81ad2a55de600b13967879c341706afb -r 100 -keylist "$keyFile"
     done
     for _ in {1..6}; do
-        go run application.go -t getHistoryForAssetRange -k 0x0c52b252da5cc314c744b7715206282bf3ba9eb4 -r 5000 -keylist "$keyFile"
+        go run application.go -t getHistoryForAssetRange -k 0x0c52b252da5cc314c744b7715206282bf3ba9eb4 -r 1000 -keylist "$keyFile"
     done
 }
 
 run_range_tests_10M() {
     local keyFile="~/Documents/insert-tpch/ethereum/10M-versions.txt"
     for _ in {1..6}; do
-        go run application.go -t getHistoryForAssetRange -k 0x0000000000022da6024a5657b6a1d7f4fff03315 -r 100 -keylist "$keyFile"
+        go run application.go -t getHistoryForAssetRange -k 0x00000000dcde63981eeb8029c620354919dc78bd -r 10 -keylist "$keyFile"
     done
     for _ in {1..6}; do
-        go run application.go -t getHistoryForAssetRange -k 0x0000005c7dc69d405f09aaadca29068d4f88cde8 -r 1000 -keylist "$keyFile"
+        go run application.go -t getHistoryForAssetRange -k 0x0000005c7dc69d405f09aaadca29068d4f88cde8 -r 100 -keylist "$keyFile"
     done
     for _ in {1..6}; do
-        go run application.go -t getHistoryForAssetRange -k 0x0000ac61d2ebe805e9de54f430075f896d0afaac -r 5000 -keylist "$keyFile"
+        go run application.go -t getHistoryForAssetRange -k 0x0000ac61d2ebe805e9de54f430075f896d0afaac -r 1000 -keylist "$keyFile"
     done
 }
 
