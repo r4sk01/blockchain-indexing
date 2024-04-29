4bef6bc8c691829b2af8140e106a22ab517488a0
Daniel Garon <dgaron@colostate.edu>
2024-04-29 10:53:13 -0600

    Updates test script

--- blockchainIndexing/testInsert.sh
+++ blockchainIndexing/testInsert.sh
@@ -8,47 +8,45 @@ IFS=$'\n\t'
 # Date: 2024-03-14
 
 main() {
-    local results=/home/andrey/Desktop/refactoringTest.txt
+    results=/home/andrey/Desktop/branchTest.txt
     local branches=(
         2.3-hlf-im-original
         2.3-hlf-im-version
         2.3-hlf-im-block
     )
     for branch in "${branches[@]}"; do
-        {
-            echo "Building images for $branch"
-            buildImages "$branch"
-            insert_and_test
-        } >> "$results" 2>&1
+        echo "Building images for $branch"
+        buildImages "$branch"
+        insert_and_test
     done
 }
 
 insert_and_test() {
     local dataFile=/home/andrey/Documents/insert-tpch/ethereum/First100K/blockTransactions17010001-17011000.json
-    ./startFabric.sh go &> /dev/null
+    ./startFabric.sh go
     sleep 10
     pushd ./go
-    printf "Inserting %s\n\n" "$dataFile"
-    go run application.go -t BulkInvokeParallel -f "$dataFile"
-    printf "\n"
-    go run application.go -t GetHistoryForKey -k 0x00000000000124d994209fbb955e0217b5c2eca1
-    go run application.go -t GetHistoryForKeyRange -k 0x00000000000124d994209fbb955e0217b5c2eca1
-    go run application.go -t GetHistoryForVersionRange -k 0x00000000000124d994209fbb955e0217b5c2eca1 -s 3 -e 6
-    go run application.go -t GetHistoryForBlockRange -s 10 -e 20 -u 3
+    {
+        printf "Inserting %s\n\n" "$dataFile"
+        go run application.go -t BulkInvokeParallel -f "$dataFile"
+        printf "\n"
+        go run application.go -t GetHistoryForKey -k 0x00000000000124d994209fbb955e0217b5c2eca1
+        go run application.go -t GetHistoryForKeyRange -k 0x00000000000124d994209fbb955e0217b5c2eca1
+        go run application.go -t GetHistoryForVersionRange -k 0x00000000000124d994209fbb955e0217b5c2eca1 -s 3 -e 6
+        go run application.go -t GetHistoryForBlockRange -s 10 -e 20 -u 3
+    } >> "$results" 2>&1
     popd
-    ./networkDown.sh &> /dev/null
+    ./networkDown.sh
     printf "\n"
 }
 
 buildImages() {
     pushd /home/andrey/Desktop/hlf-indexing-middleware
     git checkout "$1"
-    {
-        make docker-clean 
-        echo "y" | docker image prune
-        make peer-docker
-        make orderer-docker
-    } &> /dev/null
+    make docker-clean 
+    echo "y" | docker image prune
+    make peer-docker
+    make orderer-docker
     popd
 }
 
